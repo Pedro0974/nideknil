43cf92c964e55554855c41e723c5a615f2253c80
olzmoooraes <luizotaviomoraesdacruz@gmail.com>
2024-04-03 00:05:12 -0300

    deftalhes sobre a  vaga

--- App.js
+++ App.js
@@ -13,9 +13,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="VagasList">
-        <Stack.Screen name="VagasList" component={VagasList} />
-        <Stack.Screen name="VagaDetalhes" component={VagaDetalhes} />
-        <Stack.Screen name="EmpresaSobre" component={EmpresaSobre} />
+        <Stack.Screen name="Listagem de vagas" component={VagasList} />
+        <Stack.Screen name="Detalhes da vaga" component={VagaDetalhes} />
+        <Stack.Screen name="Sobre a empresa" component={EmpresaSobre} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/VagaDetalhes.js
+++ src/VagaDetalhes.js
@@ -1,24 +1,53 @@
 // VagaDetalhes.js
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import moment from "moment";
 
 const VagaDetalhes = ({ route }) => {
   const { vaga } = route.params;
   const navigation = useNavigation();
 
   const navigateToEmpresaSobre = () => {
-    navigation.navigate('EmpresaSobre');
+    navigation.navigate('Sobre a Empresa', { empresa: vaga.company, localizacao: vaga.location });
   };
 
   return (
     <View>
-      <Text>{vaga.title}</Text>
-      <Text>{vaga.company}</Text>
+      <Text style={styles.detalheTitle}>{vaga.title}</Text>
+      <Text style={styles.detalheCompany}>Nome da empresa: {vaga.company}</Text>
+      <Text style={styles.detalheSalary}>Salario: {vaga.salary}</Text>
+      <Text style={styles.detalheTimeVaga}>O tempo para a candidatura vai de {moment(vaga.datePosted).format("DD/MM/YYYY")} ate o {moment(vaga.deadline).format("DD/MM/YYYY")}</Text>
       <Button title="Sobre a Empresa" onPress={navigateToEmpresaSobre} />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  detalheTitle:{
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 30,
+    paddingTop: 20
+  },
+  detalheCompany:{
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 20
+  },
+  detalheSalary:{
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 20
+  },
+  detalheTimeVaga:{
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+});
 export default VagaDetalhes;
