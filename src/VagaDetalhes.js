744bda236581d447464858fc00f6ac6119f97507
olzmoooraes <luizotaviomoraesdacruz@gmail.com>
2024-04-03 00:25:12 -0300

    ajsutando css

--- src/VagaDetalhes.js
+++ src/VagaDetalhes.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import moment from "moment";
+import { Ionicons } from "react-native-vector-icons";
 
 const VagaDetalhes = ({ route }) => {
   const { vaga } = route.params;
@@ -16,38 +16,40 @@ const VagaDetalhes = ({ route }) => {
   return (
     <View>
       <Text style={styles.detalheTitle}>{vaga.title}</Text>
-      <Text style={styles.detalheCompany}>Nome da empresa: {vaga.company}</Text>
+      <Text style={styles.detalheCompany}>Nome da empresa: {vaga.company} 
+      <Ionicons name="briefcase" size={18} color="#666" style={{ marginLeft: 10 }} />
+      </Text>
       <Text style={styles.detalheSalary}>Salario: {vaga.salary}</Text>
-      <Text style={styles.detalheTimeVaga}>O tempo para a candidatura vai de {moment(vaga.datePosted).format("DD/MM/YYYY")} ate o {moment(vaga.deadline).format("DD/MM/YYYY")}</Text>
-      <Button title="Sobre a Empresa" onPress={navigateToEmpresaSobre} />
+      <Text style={styles.detalheTimeVaga}>O tempo para a candidatura vai de {vaga.datePosted} ate o {vaga.deadline}</Text>
+      <Button style={styles.detalheButton} title="Clique aqui para saber mais sobre quem esta te contratando" onPress={navigateToEmpresaSobre} />
     </View>
   );
 };
 const styles = StyleSheet.create({
   detalheTitle:{
-    color: '#fff',
     textAlign: 'center',
     fontSize: 30,
     paddingTop: 20
   },
   detalheCompany:{
-    color: '#fff',
     textAlign: 'center',
     fontSize: 20,
     paddingTop: 20
   },
   detalheSalary:{
-    color: '#fff',
     textAlign: 'center',
     fontSize: 20,
     paddingTop: 20
   },
   detalheTimeVaga:{
-    color: '#fff',
     textAlign: 'center',
     fontSize: 20,
     paddingTop: 20,
     paddingBottom: 20
   },
+  detalheButton:{
+    width: 100,
+    
+  }
 });
 export default VagaDetalhes;
